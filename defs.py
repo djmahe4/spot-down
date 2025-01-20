1ec1b777d6ade82c7a402e6e78aab7e6ac8797f9
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-01-20 19:34:17 +0530

    Update defs.py

--- defs.py
+++ defs.py
@@ -36,31 +36,9 @@ def process_spot_track(aname, y, match,directory,track_url,dance,tgram):
     else:
         print(f"{y} Failed test, Energy={match}, Dancebility={dance}")
 
-
-def install_ffmpeg():
-    os_type = platform.system()  # Detect the OS
-    try:
-        if os_type == "Windows":
-            print("Installing FFmpeg on Windows...")
-            # Download FFmpeg zip (change URL to the latest version)
-            os.system(
-                "curl -O https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip")
-            # Extract and set PATH (assumes you have 'tar' or a similar tool)
-            os.system("tar -xf ffmpeg-master-latest-win64-gpl.zip")
-            os.environ["PATH"] += os.pathsep + os.path.abspath("ffmpeg-master-latest-win64-gpl/bin")
-            print("FFmpeg installed successfully.")
-
-        elif os_type == "Linux":
-            print("Installing FFmpeg on Linux...")
-            os.system("sudo apt update && sudo apt install -y ffmpeg")
-            print("FFmpeg installed successfully.")
-
-        elif os_type == "Darwin":  # macOS
-            print("Installing FFmpeg on macOS...")
-            os.system("brew install ffmpeg")
-            print("FFmpeg installed successfully.")
-
-        else:
-            print(f"Unsupported OS: {os_type}")
-    except Exception as e:
-        print(f"Error during FFmpeg installation: {e}")
+def download(sp,query):
+    songs = sp.search([
+        query])
+    song, fpath = sp.download(songs[0])
+    st.write(fpath)
+    st.audio(fpath)
